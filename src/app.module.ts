01c79ecc41f5223c700f8f0b87c978b54ae26c76
Gaston Leegstra <gastonleegstra@gmail.com>
2025-08-15 18:50:51 -0300

    feat(pet): agregar módulo de mascotas al módulo principal de la aplicación
    - Se añadió el PetModule a la lista de módulos importados en app.module.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PetModule } from './pet/pet.module';
 
 @Module({
   imports: [
     AuthModule, 
-    UsersModule],
+    UsersModule, 
+    PetModule],
   controllers: [],
   providers: [],
 })
